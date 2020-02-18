a231d6b562dc1aa38c37eeac53a8f3cea0653c82
lsh <tjsghksusan@naver.com>
2020-02-18 23:11:17 +0900

    통장 정보를 담고 있는 VO class

---
+++ src/bank/BankBookVO.java
@@ -0,0 +1,23 @@
+package bank;
+
+public class BankBookVO {
+	private String bbNum;
+	private long balance;
+
+	public String getBbNum() {
+		return bbNum;
+	}
+
+	public void setBbNum(String bbNum) {
+		this.bbNum = bbNum;
+	}
+
+	public long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+
+}
