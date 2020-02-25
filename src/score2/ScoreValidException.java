64c3fd213c703aa2aece8ca5e9b6677f45ab32f1
lsh <tjsghksusan@naver.com>
2020-02-25 23:48:26 +0900

    사용자 정의 예외 - 점수는 0~100점 까지만 입력이 가능

---
+++ src/score2/ScoreValidException.java
@@ -0,0 +1,13 @@
+package score2;
+
+public class ScoreValidException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ScoreValidException(String msg) {
+		super(msg);
+	}
+	
+	
+	
+}
