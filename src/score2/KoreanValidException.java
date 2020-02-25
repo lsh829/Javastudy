40b48fab7ac257dfe44692e9af93c8bdb317da7b
lsh <tjsghksusan@naver.com>
2020-02-25 23:29:06 +0900

    사용자 정의 예외 - 이름은 한글만 입력 가능

---
+++ src/score2/KoreanValidException.java
@@ -0,0 +1,13 @@
+package score2;
+
+public class KoreanValidException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public KoreanValidException(String msg) {
+		super(msg);
+	}
+	
+	
+	
+}
